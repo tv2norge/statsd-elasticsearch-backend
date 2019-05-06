47645b103e340eb9a106a3768a97f25c31c882c7
Bjarte Brandt <bjarte.brandt@tv2.no>
2019-05-06 14:20:07 +0200

    introduce a new field 'environment' for test,stage,prod.

--- es-index-template.sh
+++ es-index-template.sh
@@ -1,185 +1,110 @@
 curl -XPUT "${ES_HOST:-localhost}:${ES_PORT:-9200}/_template/statsd-template" -d '
 {
-    "template" : "statsd-*",
+  "d2-statsd-tmpl" : {
+    "order" : 0,
+    "index_patterns" : [
+      "d2-statsd-*"
+    ],
     "settings" : {
-        "number_of_shards" : 1
+      "index" : {
+        "number_of_shards" : "1",
+        "number_of_replicas" : "1"
+      }
     },
     "mappings" : {
-        "counter" : {
-            "_source" : { "enabled" : true },
-            "properties": {
-                "@timestamp": {
-                    "type": "date"
-                },
-                "val": {
-                    "type": "double",
-                    "index": "not_analyzed"
-                },
-                "ns": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "grp": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "tgt": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "act": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                }
-            }
+      "_doc" : {
+        "_source" : {
+          "enabled" : true
         },
-        "gauge" : {
-            "_source" : { "enabled" : true },
-            "properties": {
-                "@timestamp": {
-                    "type": "date"
-                },
-                "val": {
-                    "type": "double",
-                    "index": "not_analyzed"
-                },
-                "ns": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "grp": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "tgt": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "act": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                }
-            }
-        },
-        "timer" : {
-            "_source" : { "enabled" : true },
-            "properties": {
-                "@timestamp": {
-                    "type": "date"
-                },
-                "val": {
-                    "type": "double",
-                    "index": "not_analyzed"
-                },
-                "ns": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "grp": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "tgt": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "act": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                }
-            }
-        },
-        "timer_data" : {
-            "_source" : { "enabled" : true },
-            "properties": {
-                "@timestamp": {
-                    "type": "date"
-                },
-                "count_ps": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "count": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "upper": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "lower": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "mean": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "median": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "mean": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "upper": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "std": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                 "sum": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "mean_90": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "upper_90": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "sum_90": {
-                    "type": "float",
-                    "index": "not_analyzed"
-                },
-                "bin_100": {
-                    "type": "integer",
-                    "index": "not_analyzed"
-                },
-                "bin_500": {
-                    "type": "integer",
-                    "index": "not_analyzed"
-                },
-                "bin_1000": {
-                    "type": "integer",
-                    "index": "not_analyzed"
-                },
-                "bin_inf": {
-                    "type": "integer",
-                    "index": "not_analyzed"
-                },
-                "ns": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "grp": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "tgt": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                },
-                "act": {
-                    "type": "string",
-                    "index": "not_analyzed"
-                }
-            }
+        "properties" : {
+          "environment" : {
+            "type" : "keyword"
+          },
+          "type" : {
+            "type" : "keyword"
+          },
+          "@timestamp" : {
+            "type" : "date"
+          },
+          "val" : {
+            "type" : "float",
+            "index" : false
+          },
+          "ns" : {
+            "type" : "keyword"
+          },
+          "grp" : {
+            "type" : "keyword"
+          },
+          "tgt" : {
+            "type" : "keyword"
+          },
+          "act" : {
+            "type" : "keyword"
+          },
+          "count_ps" : {
+            "type" : "float",
+            "index" : false
+          },
+          "count" : {
+            "type" : "float",
+            "index" : false
+          },
+          "upper" : {
+            "type" : "float",
+            "index" : false
+          },
+          "lower" : {
+            "type" : "float",
+            "index" : false
+          },
+          "median" : {
+            "type" : "float",
+            "index" : false
+          },
+          "mean" : {
+            "type" : "float",
+            "index" : false
+          },
+          "std" : {
+            "type" : "float",
+            "index" : false
+          },
+          "sum" : {
+            "type" : "float",
+            "index" : false
+          },
+          "mean_90" : {
+            "type" : "float",
+            "index" : false
+          },
+          "upper_90" : {
+            "type" : "float",
+            "index" : false
+          },
+          "sum_90" : {
+            "type" : "float",
+            "index" : false
+          },
+          "bin_100" : {
+            "type" : "integer",
+            "index" : false
+          },
+          "bin_500" : {
+            "type" : "integer",
+            "index" : false
+          },
+          "bin_1000" : {
+            "type" : "integer",
+            "index" : false
+          },
+          "bin_inf" : {
+            "type" : "integer",
+            "index" : false
+          }
         }
-    }
-}'
+      }
+    },
+    "aliases" : { }
+  }
+}
